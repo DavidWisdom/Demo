3c6881c1db8b76d9280fb329a2f5e8754ad18233
DavidWisdom <88563609+DavidWisdom@users.noreply.github.com>
2022-08-18 14:33:02 +0800

    Delete

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  assetsDir:'static',
-  parallel:false,
-  publicPath:'./',
-  transpileDependencies: true
-})
