131443115cc1c7a9422a7dc084eeddbcfc290463
DavidWisdom <88563609+DavidWisdom@users.noreply.github.com>
2022-08-18 20:54:29 +0800

    Update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,7 +17,7 @@
             </template>
             <el-menu-item-group>
               <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
+                    <router-link to="/about"><el-menu-item index="1-1">摄像头</el-menu-item></router-link>
               <el-menu-item index="1-2">选项2</el-menu-item>
             </el-menu-item-group>
             <el-menu-item-group title="分组2">
